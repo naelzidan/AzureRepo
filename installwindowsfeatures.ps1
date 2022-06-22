8a5ccdf9233178f81037c51c0183662a6f97c6d0
naelzidan <zidan.nael@gmail.com>
2022-06-22 15:52:11 +0300

    Add files via upload

---
+++ installwindowsfeatures.ps1
@@ -0,0 +1,7 @@
+ Install-WindowsFeature NET-Framework-Features
+ Install-WindowsFeature NET-HTTP-Activation
+ Install-WindowsFeature NET-Non-HTTP-Activ
+ Install-WindowsFeature NET-Framework-45-Features
+ Install-WindowsFeature Web-Server 
+ Install-WindowsFeature Web-Mgmt-Console
+ Install-WindowsFeature Web-Scripting-Tools
