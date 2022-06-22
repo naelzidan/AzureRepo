cf1177a4593c3df07b5aab2c06cebd3fc1c9a50d
naelzidan <zidan.nael@gmail.com>
2022-06-22 17:08:51 +0300

    Update installwindowsfeatures.ps1

--- installwindowsfeatures.ps1
+++ installwindowsfeatures.ps1
@@ -1,7 +1,8 @@
- Install-WindowsFeature NET-Framework-Features
- Install-WindowsFeature NET-HTTP-Activation
- Install-WindowsFeature NET-Non-HTTP-Activ
- Install-WindowsFeature NET-Framework-45-Features
- Install-WindowsFeature Web-Server 
- Install-WindowsFeature Web-Mgmt-Console
- Install-WindowsFeature Web-Scripting-Tools
+ Add-WindowsFeature Web-Server 
+ Add-WindowsFeature Web-Scripting-Tools
+ Add-WindowsFeature Web-Mgmt-Console
+ Add-Content -Path \"C:\\inetpub\\wwwroot\\Default.htm\" -Value $($env:computername); `
+ Add-WindowsFeature NET-Framework-45-Features
+ Add-WindowsFeature NET-Framework-Features
+ Add-WindowsFeature NET-HTTP-Activation
+ Add-WindowsFeature NET-Non-HTTP-Activ
